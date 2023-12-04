8093935a9fb5f4385ad95550d8ca716b8b4eda01
Radek Bartoň <radek.barton@microsoft.com>
2023-12-04 20:16:44 +0100

    Fix x86_64-pc-linux-gnu and aarch64-pc-linux-gnu build (#8)
    * * fix undefined references in libgfortran
    
    * * fix internal compiler error in gfortran
    
    * Revert "* fix internal compiler error in gfortran"
    
    This reverts commit 4c81782ca9e75120eb91e1b6b89559f277233621.
    
    * * revert changes for undefined references in gfortran
    
    * Fix build on x86_64-pc-linux-gnu when host is aarch64-w64-mingw32 (#6)
    
    * Fix x86_64-pc-linux-gnu and aarch64-pc-linux-gnu build
    
    ---------
    
    Co-authored-by: Evgeny Karpov <eukarpov@gmail.com>

--- gcc/common/config/aarch64/aarch64-common.cc
+++ gcc/common/config/aarch64/aarch64-common.cc
@@ -475,6 +475,8 @@ is_host_cpu_not_armv8_base (int argc, const char **argv)
   return "";
 }
 
+#if TARGET_AARCH64_MS_ABI
+
 /* Implement TARGET_EXCEPT_UNWIND_INFO.  */
 
 static enum unwind_info_type
@@ -500,6 +502,8 @@ aarch64_except_unwind_info (struct gcc_options *opts)
 
 #undef  TARGET_EXCEPT_UNWIND_INFO
 #define TARGET_EXCEPT_UNWIND_INFO  aarch64_except_unwind_info
+
+#endif // TARGET_AARCH64_MS_ABI
 
 struct gcc_targetm_common targetm_common = TARGETM_COMMON_INITIALIZER;
 
